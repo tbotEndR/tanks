205137d3ee398f474871f40f856b86d2385359b8
tbot <thibaut.endress27@proton.me>
2025-07-25 22:51:06 +0200

    rewrite to use quaternions

--- main.c
+++ main.c
@@ -17,7 +17,7 @@
 #include "rlgl.h"
 #include "raymath.h"
 
-#define TURNSPEED 0.5f
+#define TURNSPEED 0.05f
 #define DRIVESPEED 0.1f
 
 //------------------------------------------------------------------------------------
@@ -29,8 +29,10 @@ int main(void)
     //--------------------------------------------------------------------------------------
     const int screenWidth = 800;
     const int screenHeight = 450;
+    const Vector3 xAxis = { 1.0f, 0.0f, 0.0f };
     const Vector3 yAxis = { 0.0f, 1.0f, 0.0f };
     const Vector3 zAxis = {0.0f, 0.0f, 1.0f};
+    const Vector3 center = {0.0f, 0.0f, 0.0f};
 
     InitWindow(screenWidth, screenHeight, "raylib [core] example - 3d camera mode");
 
@@ -41,7 +43,6 @@ int main(void)
     camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
     camera.fovy = 60.0f;                                // Camera field-of-view Y
     camera.projection = CAMERA_PERSPECTIVE;             // Camera mode type
-
     
     //  mouse input
     Ray mouseRay = { 0 };
@@ -50,15 +51,21 @@ int main(void)
     Vector2 mouseScreenPosition = {0.0f, 0.0f};
 
     //  player body
-    Vector3 bodyPositionVect = { 0 };
-    Vector3 bodyDirectionVect = { 0 };
-    Vector3 turretPositionVect = { 0 };
-    Vector3 turretToMouseVect = { 0 };
-    float bodyRotation = 0.0f;
-    float radBodyRotation = bodyRotation;
-    float turretRotation= 0.0f;
+    Vector3 bodyPosition = { 0 };
+    Quaternion bodyRotation = QuaternionIdentity();
+    Vector3 turretPosition = { 0 };
+    Quaternion turretRotation = { 0 };
 
-    
+    //  rotation stuff
+    float degAngle = 0.5f;
+    float radAngleStep = degAngle * DEG2RAD;
+    Quaternion xRotation = QuaternionIdentity();
+    Quaternion yRotation = QuaternionIdentity();
+    Quaternion zRotation = QuaternionIdentity();
+    Quaternion stepRotation = { 0 };
+    float quaternionAngle = 0.0f;
+    Vector3 quaternionAxis = { 0 };
+    float s = 0.0f;
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
@@ -70,35 +77,42 @@ int main(void)
         //----------------------------------------------------------------------------------
         // TODO: Update your variables here
         //----------------------------------------------------------------------------------
-        turretPositionVect = (Vector3) {bodyPositionVect.x, bodyPositionVect.y+1.0f, bodyPositionVect.z};
+
+        //  rotate cube
+        /*
+        xRotation = QuaternionFromAxisAngle(xAxis, radAngleStep);
+        yRotation = QuaternionFromAxisAngle(yAxis, radAngleStep);
+        zRotation = QuaternionFromAxisAngle(zAxis, radAngleStep);
+        stepRotation = QuaternionMultiply(zRotation, QuaternionMultiply(yRotation, xRotation));
+        bodyRotation = QuaternionNormalize(QuaternionMultiply(stepRotation, bodyRotation));
+        */
+
+        //  get axis and angle from updated rotation
+        quaternionAngle = (2.0f * acosf(Clamp(bodyRotation.w, -1.0f, 1.0f))) * RAD2DEG;
+        s = sqrt(1 - (bodyRotation.w*bodyRotation.w));
+        if (s < 0.001f) quaternionAxis = (Vector3) {1.0f, 0.0f, 0.0f};
+        else {
+            quaternionAxis.x = bodyRotation.x / s;
+            quaternionAxis.y = bodyRotation.y / s;
+            quaternionAxis.y = bodyRotation.y / s;
+        }
+
         //  raycast from mouse to collide with invisible quad to get world position
         mouseScreenPosition = GetMousePosition();
         mouseRay = GetScreenToWorldRay(GetMousePosition(), camera);
         collision = GetRayCollisionBox(mouseRay, mousePointerBox);
 
-        //  calculate rotation angle of the turret
-        turretToMouseVect = Vector3Normalize(Vector3Subtract(turretPositionVect, collision.point));
-        turretRotation = Vector3Angle(turretToMouseVect, zAxis) * RAD2DEG;
-        if (turretToMouseVect.x < 0) turretRotation *=-1;
-
         //  body angle
         if (IsKeyDown(KEY_A)) {
-            bodyRotation -= TURNSPEED;
-            if (bodyRotation >= 360) bodyRotation = 0;
-            radBodyRotation = bodyRotation * DEG2RAD;
-            bodyDirectionVect.x = cos(radBodyRotation);
-            bodyDirectionVect.z = sin(radBodyRotation);
+            yRotation = QuaternionFromAxisAngle(yAxis, TURNSPEED);
+            bodyRotation = QuaternionNormalize(QuaternionMultiply(yRotation, bodyRotation));
         }
         if (IsKeyDown(KEY_D)) {
-            bodyRotation += TURNSPEED;
-            if (bodyRotation < 0) bodyRotation = 360;
-            radBodyRotation = bodyRotation * DEG2RAD;
-            bodyDirectionVect.x = cos(radBodyRotation);
-            bodyDirectionVect.z = sin(radBodyRotation);
+            yRotation = QuaternionFromAxisAngle(yAxis, -TURNSPEED);
+            bodyRotation = QuaternionNormalize(QuaternionMultiply(yRotation, bodyRotation));
         }
 
-        
-
+        /*
         //  movement
         if (IsKeyDown(KEY_W)) {
             bodyPositionVect = Vector3Add(bodyPositionVect, Vector3Scale(bodyDirectionVect, DRIVESPEED));
@@ -106,16 +120,12 @@ int main(void)
         if (IsKeyDown(KEY_S)) {
             bodyPositionVect = Vector3Add(bodyPositionVect, Vector3Scale(bodyDirectionVect, -DRIVESPEED));
         }
+            */
 
         if (IsKeyPressed(KEY_R)) {
-            bodyPositionVect = (Vector3){ 0.0f, 0.0f, 0.0f };
-            bodyDirectionVect = (Vector3) {1.0f, 0.0f, 0.0f };
-            turretPositionVect = (Vector3){ 0 };
-            turretToMouseVect = (Vector3) { 0 };
-            bodyRotation = 0.1f;
-            turretRotation= 0.0f;
+            bodyPosition =  center;
+            bodyRotation =QuaternionIdentity();
         }
-
 
 
         // Draw
@@ -125,32 +135,20 @@ int main(void)
             ClearBackground(RAYWHITE);
 
             BeginMode3D(camera);
-
-                
-                //DrawLine3D(turretPositionVect, collision.point, BLUE);
-                DrawLine3D(bodyPositionVect, Vector3Add(bodyPositionVect, Vector3Scale(bodyDirectionVect, 20.0f)), BLUE);
-                DrawCube(bodyPositionVect, 1.0f, 0.5f, 1.0f, BLUE);
-                DrawCubeWires(bodyPositionVect, 1.0f, 0.5f, 1.0f, RAYWHITE);                
-
-                /*
-                rlPushMatrix();
-                    rlTranslatef(bodyPositionVect.x, 0.0f, bodyPositionVect.z);
-                    rlRotatef(bodyRotation, 0.0f, 1.0f, 0.0f);
-                    DrawCube(bodyPositionVect, 2.0f, 1.0f, 2.0f, RED);
-                    DrawCubeWires(bodyPositionVect, 2.0f, 1.0f, 2.0f, MAROON);
-                rlPopMatrix();
-                */
-
                 DrawGrid(30, 1.0f);
+                rlPushMatrix();
+                    rlTranslatef(bodyPosition.x, bodyPosition.y, bodyPosition.z);
+                    rlRotatef(quaternionAngle, quaternionAxis.x, quaternionAxis.y, quaternionAxis.z);
+                    DrawCubeWires(center, 5.0f, 5.0f, 5.0f, BLUE);
+                rlPopMatrix();
 
             EndMode3D();
 
-            //DrawText(TextFormat("%.2f|%.2f", collision.point.x, collision.point.z), mouseScreenPosition.x+10, mouseScreenPosition.y-10, 20, GREEN);
+            DrawText(TextFormat("%.2f|%.2f", mouseScreenPosition.x, mouseScreenPosition.y), mouseScreenPosition.x+10, mouseScreenPosition.y-10, 20, GREEN);
             //DrawText(TextFormat("turretToMouseVect: {%.2f, %.2f, %.2f}", turretToMouseVect.x, turretToMouseVect.y, turretToMouseVect.z), 10, 20, 10, BLACK);
-            DrawText(TextFormat("bodyDirectionVect: {%.2f, %.2f, %.2f}", bodyDirectionVect.x, bodyDirectionVect.y, bodyDirectionVect.z), 10, 40, 10, BLACK);
-            DrawText(TextFormat("bodyPositionVect: {%.2f, %.2f, %.2f}", bodyPositionVect.x, bodyPositionVect.y, bodyPositionVect.z), 10, 60, 10, BLACK);
-            DrawText(TextFormat("bodyRotation: {%.2f}", bodyRotation), 10, 80, 10, BLACK);
-
+            //DrawText(TextFormat("bodyDirectionVect: {%.2f, %.2f, %.2f}", bodyDirectionVect.x, bodyDirectionVect.y, bodyDirectionVect.z), 10, 40, 10, BLACK);
+            //DrawText(TextFormat("bodyPositionVect: {%.2f, %.2f, %.2f}", bodyPositionVect.x, bodyPositionVect.y, bodyPositionVect.z), 10, 60, 10, BLACK);
+            //DrawText(TextFormat("bodyRotation {%.2f |%.2f | %.2f | %.2f}", bodyRotation.w, bodyRotation.x, bodyRotation.y, bodyRotation.z), 10, 80, 10, BLACK);
 
             DrawFPS(5, 5);
 
